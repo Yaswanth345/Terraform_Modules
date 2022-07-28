eb0e23f3964e08c7d8a943578650ef0618ce34e6
Yaswanth <yaswanthgopi918@gmail.com>
2022-07-28 22:11:10 +0530

    second commit

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
-output "subnet_name" {
+output "vpcid" {
 
     description = "Subnets Names"
-    value = [aws_subnet.private_subnet.tags.Name, aws_subnet.private_subnet.id , aws_subnet.public_subnet.tags.Name, aws_subnet.public_subnet.id]
+    value = [aws_vpc.main.id]
 }
